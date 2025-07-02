3907e2ccea2d1cc8d07bd62f3f68b178798b3a86
sg296858 <you@example.com>
2025-07-02 14:08:41 +0530

    first version of data saved

---
+++ mycode.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import os
+
+data={'Name':['Alice','Bob','Charlie'],
+      'Age':[20,25,30],
+      'City':['NewYork','Charlie','Bob']
+      }
+
+df=pd.DataFrame(data)
+
+data="dir"
+os.makedirs(data,exist_ok=True)
+
+filepath=os.path.join(data,'sample-data.csv')
+
+# Save the DataFrame to a CSV file, including column names
+df.to_csv(filepath, index=False)
+
+print(f"CSV file saved to {filepath}")
